ad2e7c91ecce132c5cf738fc7e8e139717233e32
Jacob <jacob@Jacobs-MacBook-Air.local>
2017-01-31 16:19:25 -0500

    sql files are done and handlebars files were started

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Donut Burger',0);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Pizza Burger',0);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Taco Burger',0);
